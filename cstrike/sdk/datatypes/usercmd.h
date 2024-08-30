2632a4c3975c96fb06cb1a2d231dd3b5d9da9a43
Exlodium <45338028+Exlodium@users.noreply.github.com>
2024-08-31 00:55:30 +0200

    update buttons enum fixing #119

--- cstrike/sdk/datatypes/usercmd.h
+++ cstrike/sdk/datatypes/usercmd.h
@@ -8,36 +8,26 @@
 #include "../../core/interfaces.h"
 #include "../interfaces/imemalloc.h"
 
-// @source: master/game/shared/in_buttons.h
-enum ECommandButtons : int
+// @source: server.dll
+enum ECommandButtons : std::uint64_t
 {
-	IN_ATTACK = (1 << 0),
-	IN_JUMP = (1 << 1),
-	IN_DUCK = (1 << 2),
-	IN_FORWARD = (1 << 3),
-	IN_BACK = (1 << 4),
-	IN_USE = (1 << 5),
-	IN_CANCEL = (1 << 6),
-	IN_LEFT = (1 << 7),
-	IN_RIGHT = (1 << 8),
-	IN_MOVELEFT = (1 << 9),
-	IN_MOVERIGHT = (1 << 10),
-	IN_SECOND_ATTACK = (1 << 11),
-	IN_RUN = (1 << 12),
-	IN_RELOAD = (1 << 13),
-	IN_LEFT_ALT = (1 << 14),
-	IN_RIGHT_ALT = (1 << 15),
-	IN_SCORE = (1 << 16),
-	IN_SPEED = (1 << 17),
-	IN_WALK = (1 << 18),
-	IN_ZOOM = (1 << 19),
-	IN_FIRST_WEAPON = (1 << 20),
-	IN_SECOND_WEAPON = (1 << 21),
-	IN_BULLRUSH = (1 << 22),
-	IN_FIRST_GRENADE = (1 << 23),
-	IN_SECOND_GRENADE = (1 << 24),
-	IN_MIDDLE_ATTACK = (1 << 25),
-	IN_USE_OR_RELOAD = (1 << 26)
+	IN_ATTACK = 1 << 0,
+	IN_JUMP = 1 << 1,
+	IN_DUCK = 1 << 2,
+	IN_FORWARD = 1 << 3,
+	IN_BACK = 1 << 4,
+	IN_USE = 1 << 5,
+	IN_LEFT = 1 << 7,
+	IN_RIGHT = 1 << 8,
+	IN_MOVELEFT = 1 << 9,
+	IN_MOVERIGHT = 1 << 10,
+	IN_SECOND_ATTACK = 1 << 11,
+	IN_RELOAD = 1 << 13,
+	IN_SPRINT = 1 << 16,
+	IN_JOYAUTOSPRINT = 1 << 17,
+	IN_SHOWSCORES = 1ULL << 33,
+	IN_ZOOM = 1ULL << 34,
+	IN_LOOKATWEAPON = 1ULL << 35
 };
 
 // compiled protobuf messages and looked at what bits are used in them
